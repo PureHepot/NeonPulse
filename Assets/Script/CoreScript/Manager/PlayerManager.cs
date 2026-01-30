686668ddab67d3e1dbdfef99cbeecbd637e4d0a2
GDi <desripure66@gmail.com>
2026-01-30 22:51:49 +0800

    1

--- Assets/Script/CoreScript/Manager/PlayerManager.cs
+++ Assets/Script/CoreScript/Manager/PlayerManager.cs
@@ -65,7 +65,7 @@ public class PlayerManager : MonoSingleton<PlayerManager>
 
         this.PlayerPreview = GameObject.Find("PlayerModelCamera").GetComponent<PlayerPreview>();
 
-        UpgradeManager.Instance.ApplyModulesToPlayer(this.PlayerPreview.CurrentModel.GetComponent<PlayerModuleManager>());
+        //UpgradeManager.Instance.ApplyModulesToPlayer(this.PlayerPreview.CurrentModel.GetComponent<PlayerModuleManager>());
 
         CurrentModules.Initialize?.Invoke();
 
